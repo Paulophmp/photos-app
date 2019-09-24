1f7e7b2b9d3428f7ad30860ae4dcef3f52bb377a
PAULINHO <paulo.mendes00@hotmail.com>
2019-09-24 15:48:43 -0300

    [UPDATE] Inciando o projeto
    Co-authored-by: Thiago <thiagobfiorenza@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/gato01.jpg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
